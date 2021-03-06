b573f78b7a4c486291b58bd6b73c0adf13272c10
Lamine Ndiaye <lamine.ndy@gmail.com>
2021-03-06 22:29:13 +0100

    Initial Commit

---
+++ AnimeUI/AnimeUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimeUIApp.swift
+//  AnimeUI
+//
+//  Created by Ndiaye, Lamine on 06/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct AnimeUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
